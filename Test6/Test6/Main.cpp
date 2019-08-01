ea1f68b5ded9f543e208b19aff0b7431513b31ca
jangbigom91 <jangbigom91@gmail.com>
2019-08-01 20:09:19 +0900

    19.08.01

--- Test6/Test6/Main.cpp
+++ Test6/Test6/Main.cpp
@@ -5,6 +5,19 @@
 
 /*****포안터 배열*****/
 
+void output(char *p, int arraysize)
+{
+	int i = 0;
+
+	for (i = 0; i < arraysize; i++)
+	{
+		if (p[i] == '\n') {
+			break;
+		}
+		printf("%c", p[i]);
+	}
+}
+
 int main(void)
 {
 	/*
@@ -41,18 +54,52 @@ int main(void)
 
 	return 0;
 	*/
+	
+	/**/
+	int i = 0;
+	char array1[20] = {NULL};
+	char* p = NULL;
 
+	array1[0] = 'H';
+	array1[1] = 'E';
+	array1[2] = 'L';
+	array1[3] = 'L';
+	array1[4] = 'O';
+	array1[5] = ' ';
+	array1[6] = 'W';
+	array1[7] = 'O';
+	array1[8] = 'R';
+	array1[9] = 'L';
+	array1[10] = 'D';
+	array1[11] = '\n';
 
+	
+	for (i = 0; i < sizeof(array1); i++)
+	{
+		if (array1[i] == '\n')
+		{
+			break;
+		}
+		printf("%c", array1[i]);
 
+	}
 
+	printf("\n-----\n");
 
+	p = array1;
 
+	for (i = 0; i < sizeof(array1); i++)
+	{
+		if (p[i] == '\n') {
+			break;
+		}
+		printf("%c", p[i]);
+	}
 
+	printf("\n-----\n");
 
-
-
-
-
+	int size = sizeof(array1);
+	output(array1, size);
 
 
 
