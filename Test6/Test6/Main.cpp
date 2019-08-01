131e6ceaeb37b6cad0cebe3bcd71b09770b2673c
jangbigom91 <jangbigom91@gmail.com>
2019-08-01 19:11:52 +0900

    11

---
+++ Test6/Test6/Main.cpp
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+
+/*****포안터 배열*****/
+
+int main(void)
+{
+	/*
+	int a = 10, b = 20, c = 30;
+	int* ap[3]; ///포인터 배열///
+
+	int array[2][3] = { 10,20,30,40,50,60 };
+	int(*p)[3]; ///배열 포인터///
+
+	ap[0] = &a;
+	ap[1] = &b;
+	ap[2] = &c;
+
+	p = array;
+	*/
+
+	/*****포인터와 문자 그리고 포인터와 문자열*****/
+
+	/*
+	char array[] = { 'A','B','C','D' };
+	///문자상수///
+	printf("문자 상수 : %c %c %c %c\n", 'A', 'B', 'C', 'D');
+
+	///문자배열///
+	printf("문자 배열 변경 전 : %c %c %c %c\n", array[0], array[1], array[2], array[3]);
+
+	array[0] = 'D';
+	array[1] = 'C';
+	array[2] = 'B';
+	array[3] = 'A';
+
+	printf("문자 배열 변경 후 : %c %c %c %c\n", array[0], array[1], array[2], array[3]);
+	printf("문자 배열 array의 크기 : %d\n", sizeof(array));  ///문자 배열의 크기 4
+
+	return 0;
+	*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
